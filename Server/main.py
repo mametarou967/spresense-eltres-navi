ad9dcd03b40f61a2b1fcf072b8daa9fa188347d1
mametarou963 <mametarou967@gmail.com>
2022-09-24 18:40:41 +0900

    Update main.py

--- Server/main.py
+++ Server/main.py
@@ -5,25 +5,19 @@ from typing import Union,Set
 
 app = FastAPI()
 
-class TudeValue(BaseModel):
-  degrees: str
-  minutes: str
-  seconds: str
-
 # リクエストbodyを定義
 class GpsDataSet(BaseModel):
-  latitude: Union[TudeValue, None] = None
-  longitude: Union[TudeValue, None] = None
-#  timeStamp: str
-
-class Hello(BaseModel):
-  hello: str
+  latDegrees: str
+  latMinutes: str
+  latSeconds: str
+  longDegrees: str
+  longMinutes: str
+  longSeconds: str
 
 # シンプルなJSON Bodyの受け取り
 @app.post("/")
 # 上で定義したUserモデルのリクエストbodyをuserで受け取る
 # user = {"user_id": 1, "name": "太郎"}
-def create_user(item: TudeValue):
-  print(item.degrees)
-  # レスポンスbody
+def create_user(item: GpsDataSet):
+  print(item.latDegrees)
   return "hello"
