5a95adeed6e27181b3b944cff6d1143e543dfd83
mametarou963 <mametarou967@gmail.com>
2022-09-24 16:45:22 +0900

    Create main.py

---
+++ Server/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from pydantic import BaseModel  # リクエストbodyを定義するために必要
+from typing import List  # ネストされたBodyを定義するために必要
+from typing import Union,Set
+
+app = FastAPI()
+
+class TudeValue(BaseModel):
+  degrees: int
+  minutes: int
+  seconds: int
+
+# リクエストbodyを定義
+class GpsDataSet(BaseModel):
+  latitude: Union[TudeValue, None] = None
+  longitude: Union[TudeValue, None] = None
+#  timeStamp: str
+
+class Hello(BaseModel):
+  hello: str
+
+# シンプルなJSON Bodyの受け取り
+@app.post("/")
+# 上で定義したUserモデルのリクエストbodyをuserで受け取る
+# user = {"user_id": 1, "name": "太郎"}
+def create_user(item: Hello):
+  print(item.hello)
+  # レスポンスbody
+  return "hello"
